728385cec95f0366030b4d47e5d016ce427773d6
김충현 <winwiewie@gmail.com>
2025-10-31 18:19:43 +0900

    로또 클래스에 검증하는 메서드들 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,18 +1,39 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        validateNullOrEmpty(numbers);
         validate(numbers);
+        validateDuplication(numbers);
+
         this.numbers = numbers;
+    }
+
+    private void validateNullOrEmpty(List<Integer> numbers) {
+        if (numbers == null) {
+            throw new IllegalArgumentException("[ERROR] 로또가 없습니다.");
+        }
+
+        if (numbers.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 로또가 없습니다.");
+        }
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        HashSet<Integer> hashNumbers = new HashSet<>(numbers);
+        if(numbers.size() != hashNumbers.size()){
+            throw new IllegalArgumentException("[ERROR] 로또번호가 중복 입니다.");
         }
     }
 
