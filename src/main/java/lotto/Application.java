a21b823ca2bbbabab055fa648543055ee0728d0c
김충현 <winwiewie@gmail.com>
2025-10-31 18:04:01 +0900

    구입 금액 을 입력시 검증하는 메서드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,51 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        String cashStrInput = Console.readLine();
+        int cash = cashValidate(cashStrInput);
     }
+
+    private static int cashValidate(String cashStrInput) {
+
+        if (cashStrInput == null) {
+            throw new IllegalArgumentException();
+        }
+
+        cashStrInput = cashStrInput.trim();
+        if (cashStrInput.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        if (cashStrInput.startsWith("0")) {
+            throw new IllegalArgumentException();
+        }
+
+        String trueStr = "^[0-9]*$";
+        if (!cashStrInput.matches(trueStr)) {
+            throw new IllegalArgumentException();
+        }
+
+        int cash = Integer.parseInt(cashStrInput);
+
+        if (cash < 1000 ) {
+            throw new IllegalArgumentException();
+        }
+
+        if (cash > 50001) {
+            throw new IllegalArgumentException();
+        }
+        System.out.println();
+
+        return cash;
+    }
+
+
+
 }
