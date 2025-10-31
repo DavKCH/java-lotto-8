18ba229b311569af03cd9726acb5f2d45c473d32
김충현 <winwiewie@gmail.com>
2025-10-31 18:13:31 +0900

    로또 당첨번호를 입력하고 검증하는 메서드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,8 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class Application {
@@ -24,6 +26,12 @@ public class Application {
             lottoRepository.add(new Lotto(numbers));
         }
         lottoRepository.lottoCatalog();
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winNumStrInput = Console.readLine();
+
+        int[] winNumbers = lottoWinNumValidate(winNumStrInput);
+
     }
 
     private static int cashValidate(String cashStrInput) {
@@ -70,6 +78,69 @@ public class Application {
 
         int lottoBuyCount = (int) decimalPoint;
         return lottoBuyCount;
+    }
+
+    private static int[] lottoWinNumValidate(String winNumStrInput) {
+        final String DELIMITER = ",";
+        final int WIN_NUMBER_COUNT = 6;
+
+        if (winNumStrInput == null) {
+            throw new IllegalArgumentException();
+        }
+
+        winNumStrInput = winNumStrInput.trim();
+        if (winNumStrInput.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        if (winNumStrInput.startsWith(DELIMITER)) {
+            throw new IllegalArgumentException();
+        }
+
+        String lastWinNumInput = winNumStrInput.substring(winNumStrInput.length()-1);
+        if (lastWinNumInput.equals(DELIMITER)) {
+            throw new IllegalArgumentException();
+        }
+
+        String[] winNumStr = winNumStrInput.split(DELIMITER);
+        for (String strNum : winNumStr) {
+            String trimNum = strNum.trim();
+            if (trimNum.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        String trueStr = "^[0-9]*$";
+        if (winNumStrInput.matches(trueStr)) {
+            throw new IllegalArgumentException();
+        }
+
+        String[] strNumbers = winNumStrInput.split(DELIMITER);
+        List<String> numbers = Arrays.asList(strNumbers);
+        HashSet<String> hashSet = new HashSet<>(numbers);
+        if (numbers.size() != hashSet.size()) {
+            throw new IllegalArgumentException();
+        }
+
+        if (strNumbers.length != WIN_NUMBER_COUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        int[] winNumbers = new int[WIN_NUMBER_COUNT];
+
+        for (int i = 0; i < WIN_NUMBER_COUNT; i++) {
+            winNumbers[i] = Integer.parseInt(strNumbers[i]);
+        }
+
+        for (int winNumber : winNumbers) {
+            if (winNumber >= 46) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        System.out.println();
+
+        return winNumbers;
     }
 
 }
